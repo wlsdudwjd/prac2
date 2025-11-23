06b14bada89197df3f1735bb06cbb5763e84be71
wlsdudwjd <jjyabc11@naver.com>
2025-11-24 02:04:14 +0900

    first commit

---
+++ src/main/java/com/example/prac2/Prac2Application.java
@@ -0,0 +1,13 @@
+package com.example.prac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prac2/Prac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
